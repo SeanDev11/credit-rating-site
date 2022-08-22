4787f2bb95f787cbf18f6ef5e4fb018c938ea3f9
SeanDev11 <sean.devine24@googlemail.com>
2022-08-23 00:43:47 +0200

    Fixed stale element issue by waiting for reload.

--- load_sp.py
+++ load_sp.py
@@ -4,12 +4,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 from pymongo import MongoClient
+import csv
+
+# TODO refactor into functions
+file = open("sp500_constituents.csv")
+csvreader = csv.reader(file)
+next(csvreader)
+companies = []
+for row in csvreader:
+    companies.append(row)
 
 # Credentials
-USERNAME = "sean.devine24@gmail.com"
-PWD = "Full$tack2022!"
+
 # Chromedriver must be in PATH
 DRIVER_PATH = "/Users/seandevine/Desktop/Life/PersonalProjects/credit-rating-site/chromedriver"
 
@@ -33,18 +42,13 @@ submit = driver.find_element_by_xpath("//button[@type='submit']").click()
 if (len(driver.find_elements_by_xpath("//button[@id='onetrust-accept-btn-handler']")) > 0):
     cookies = driver.find_element_by_xpath("//button[@id='onetrust-accept-btn-handler']").click()
 
-# Allow for popup to disappear
-#time.sleep(1.5)
-
-companies = ["APPLE", "MICROSOFT", "DUCK CREEK"]
-wait = WebDriverWait(driver, timeout=10, poll_frequency=0.25)
+wait = WebDriverWait(driver, timeout=3, poll_frequency=0.25)
 
 allRatings = []
 
-# NEED TO INCLUDE WAITS!
 for company in companies:
     ratingsList = []
-    #print(company)
+    company = company[1]
     # Go to next company
     next_search = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[starts-with(@class, 'button__search')]")))
     next_search.click()
@@ -56,52 +60,52 @@ for company in companies:
     try:
         wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='table-module__row']")))
     except TimeoutException:
-        print("NOT FOUND")
         continue
-    print("dont print after not found")
     if (len(driver.find_elements_by_xpath("//div[@class='row results-header']")) > 0):
-        #if (len(driver.find_elements_by_xpath("//div[@class='table-module__column']/p/a")) > 0):
         company = driver.find_element_by_xpath("//div[@class='table-module__column']/p/a").click()
 
     # NOW ABLE TO - find sub elements. refactor to use By.XXX, wait for proper elements perhaps sub elements
 
-    # Extract data, store in MongoDB should be done foreach from a list of companies.
-    # wait.until(EC.visibility_of_element_located((By.ID, "page1")))
     wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='table-module__row']")))
-    #ratings = driver.find_elements_by_xpath("//div[@class='table-module__row']")
-    ratings = driver.find_element(By.ID, "page1").find_elements(By.CLASS_NAME, "table-module__row")
-    print(f"RATING COUNT {len(ratings)}")
     wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "table-module__column")))
-    #wait.until(EC.presence_of_element_located((By.CLASS_NAME, "content")))
-    company_name = driver.find_element(By.XPATH, "//div[@class='content']/h1").text
-  
-    for rating in ratings:
-        info = rating.find_elements(By.CLASS_NAME, "table-module__column")
-        info_dict = {"company": company_name, "ratingType": info[0].text,
-                     "rating": info[1].find_element(By.XPATH, "//h5").text,
-                     "ratingDate": info[2].text, "lastReviewDate": info[3].text,
-                     "regulatoryIdentifiers": info[4].text, "outlook": info[5].text,
-                     "outlookDate": info[6].text}
-        ratingsList.append(info_dict)
-        #for i in info:
-         #   print(i.text)
-        #print("-----------")
-    allRatings.append({company_name: ratingsList})
 
-print(allRatings)
+    company_name = driver.find_element(By.XPATH, "//div[@class='content']/h1").text
+    try:
+        ratings = driver.find_element(By.ID, "page1").find_elements(By.CLASS_NAME, "table-module__row")
+    except:
+        continue
+
+    ratingsCount = len(ratings)
+    idx = 0
+    while (idx < ratingsCount):
+        try:
+            info = ratings[idx].find_elements(By.CLASS_NAME, "table-module__column")
+            info_dict = {"company": company_name, "ratingType": info[0].text,
+                         "rating": info[1].find_element(By.XPATH, "//h5").text,
+                         "ratingDate": info[2].text, "lastReviewDate": info[3].text,
+                         "regulatoryIdentifiers": info[4].text, "outlook": info[5].text,
+                         "outlookDate": info[6].text}
+        except StaleElementReferenceException:
+            wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='table-module__row']")))
+            wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "table-module__column")))
+            ratings = driver.find_element(By.ID, "page1").find_elements(By.CLASS_NAME, "table-module__row")
+            continue
+        
+        ratingsList.append(info_dict)
+        idx += 1
+        
+    allRatings.append({company_name: ratingsList})
 
 # Done scraping data
 time.sleep(5)
 
 # Insert data into mongoDB
-cluster = "mongodb+srv://dbAdmin:dbAdmin%24@creditrating.nijkfp8.mongodb.net/?retryWrites=true&w=majority"
+cluster = "mongodb+srv://dbAdmin:dbAdmin@creditrating.nijkfp8.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(cluster)
 # client.SPratings is prod db
 db = client.CreditRating
 ratings = db.ratings
-
 ratings.insert_many(allRatings)
-
 
 
 
